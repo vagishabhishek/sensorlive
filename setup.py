68f80d082ea00305a5e52aa5fa9809ebdc51dea9
vagishabhishek <vagish.abhishek@gmail.com>
2024-05-31 07:22:13 +0530

    fixed typing error in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
-from typing import list, str
+from typing import List
 
 
-def get_requirements() -> list[str]:
-    requirements_list = list[str] = []
+def get_requirements() -> List[str]:
+    requirements_list :List[str] = []
     return requirements_list
 
 
